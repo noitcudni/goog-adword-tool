4d6d5888da56a4781ea326ebb8536abbcd9d6a8b
Lih Chen <lihster@gmail.com>
2015-05-13 21:54:08 -0700

    it was a utf16 issue.

--- src/seo_tools/core.cljs
+++ src/seo_tools/core.cljs
@@ -18,7 +18,8 @@
    :competition  4
    :suggested_bid  5 })
 
-(def csv-filename "/Users/bdcoe/Documents/workspace/seo-tools/chinese_vocab_builder.csv")
+;(def csv-filename "/Users/bdcoe/Documents/workspace/seo-tools/chinese_vocab_builder.csv")
+(def csv-filename "/Users/bdcoe/Documents/workspace/seo-tools/test.csv")
 
 (defn header-order-lst [m]
   (vec (for [[header-key, index] m] header-key)))
@@ -54,7 +55,9 @@
 
 
 (defn open-file[filename]
-  (<<< .readFile fs filename))
+  ;(<<< .readFile fs  "utf-8 " filename)
+  (<<< .readFile fs filename "utf8")
+  )
 
 ;(defn parse-file [filename]
   ;(let [cb-chan (chan)]
@@ -66,12 +69,56 @@
             ;(println content))))
     ;))
 
+;(defn big-to-little-endian [data-s]
+  ;(let [m (map-indexed vector data-s)
+        ;even-m (map second (filter #(-> % first even?) m))
+        ;odd-m (map second (filter #(-> % first odd?) m))]
 
-(defn parse-csv [data, h-map]
+    ;(do (println "wtf>>")
+        ;(println m)
+        ;(s/join (flatten (map list odd-m even-ms))))
+    ;)
+  ;)
+
+
+(defn parse-csv [data, h-map, filter-regex]
   (let [lines (rest (s/split data #"\n"))
         header-lst (header-order-lst h-map)
-        rows-cols (map #(s/split % #"\t") lines)]
-    (println (map #(zipmap header-lst %) rows-cols))))
+        rows-cols-raw (map #(s/split % #"\t") lines)
+        rows-cols (map #(zipmap header-lst %) rows-cols-raw) ]
+
+    ;(println (re-find #"(?i)chinese" (:keyword {:keyword "Chinese foo"})))
+    ;(println (type data))
+
+
+    ;(println (= "Vocab" (s/join (map second (filter #(odd? (first %)) (map-indexed vector (nth (nth rows-cols-raw 0) 0) ))) )))
+    ;(pr-str (s/split "Vocab" ""))
+
+
+
+    ;(loop [i 0]
+      ;(if (< i  (count rows-cols))
+        ;(do
+          ;;(println (= "vocab builders" (str "" (:keyword (nth rows-cols i)))))
+          ;(println (:keyword (nth rows-cols i)))
+          ;(println (nth (s/split (:keyword (nth rows-cols i)) "") 1))
+
+          ;(recur (inc i)))
+        ;)
+      ;)
+
+    ;(println (:keyword (nth rows-cols 2)))
+    ;(println (#(:keyword %) (nth rows-cols 0)))
+
+    ;(println (filter #(and (-> % :keyword string?)
+                           ;) rows-cols))
+
+    (println (filter #(->> % :keyword (re-find filter-regex) nil? not) rows-cols))
+    ;(println rows-cols)
+
+    ;(println (filter #(nil? (re-find filter-regex  (:ad_group %))) rows-cols))
+    )
+)
 
 
 (defn -main []
@@ -82,8 +129,19 @@
   ;(println (macroexpand '(<<< .readFile fs csv-filename)))
   ;(m/foobar); test a standalone module
 
-  (go (parse-csv (<! (open-file csv-filename)) header-map))
-  ;(println (parse-file-handler (parse-file csv-filename)))
+  (go (parse-csv
+        (<! (open-file csv-filename))
+        header-map
+        #"(?i)chinese"))
+
+  ;(.readFile fs csv-filename "utf16le"
+             ;(fn [e, d]
+               ;(do
+                 ;(println (type d))
+                 ;(println d)
+                 ;(println (map-indexed vector d)))
+               ;))
   )
+
 
 (set! *main-cli-fn* -main)
